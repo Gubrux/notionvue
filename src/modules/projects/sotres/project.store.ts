065896b60f31b3712a32ac9cadbc4d986f9dae3f
gabriel <gabrielcentur@gmail.com>
2024-09-03 15:11:42 -0400

    feat: Add functionality to add projects in project store

--- src/modules/projects/sotres/project.store.ts
+++ src/modules/projects/sotres/project.store.ts
@@ -1,26 +1,30 @@
 import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import type { Project } from '../interfaces/project.interface';
-
-const initialLoad = (): Project[] => {
-    return [
-        {
-            id: '1',
-            name: 'Project 1',
-            tasks: [],
-        },
-        {
-            id: '2',
-            name: 'Project 2',
-            tasks: [],
-        },
-    ];
-};
+import { v4 as uuidv4 } from 'uuid';
+import { useLocalStorage } from '@vueuse/core';
 
 export const useProjectStore = defineStore('projects', () => {
-    const projects = ref<Project[]>(initialLoad());
+    const projects = ref(useLocalStorage<Project[]>('projects', []));
+
+    const addProject = (name: string) => {
+        if (name.length === 0) return;
+
+        projects.value.push({
+            id: uuidv4(),
+            name: name,
+            tasks: [],
+        });
+    };
 
     return {
+        // properties
         projects,
+        // getters son para obtener valores computados de las propiedades del store (como un computed) y no mutan el estado del store (no se pueden modificar) y se acceden como funciones (no como propiedades) en los componentes que los usan (por eso se llaman como funciones) y se pueden usar para filtrar, ordenar, etc.
+        projectList: computed(() => [...projects.value]),
+        noProjects: computed(() => projects.value.length === 0),
+
+        // actions
+        addProject,
     };
 });
