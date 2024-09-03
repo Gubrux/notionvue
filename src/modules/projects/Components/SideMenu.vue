73ae396407a584cf77756e972180377f845e9356
gabriel <gabrielcentur@gmail.com>
2024-09-03 15:11:37 -0400

    feat: Update SideMenu component to display project list with tasks

--- src/modules/projects/Components/SideMenu.vue
+++ src/modules/projects/Components/SideMenu.vue
@@ -1,33 +1,42 @@
 <template>
     <aside class="bg-base-200 w-72 min-h-screen">
         <h2 class="text-lg font-bold mx-4">Proyectos</h2>
-        <p class="text-sm to-gray-500 mx-4">No hay proyectos</p>
-        <ul class="menu bg-base-200 rounded-box w-56">
-            <li><a>Item 1</a></li>
-            <li>
-                <details open>
-                    <summary>Parent</summary>
-                    <ul>
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                        <li>
-                            <details open>
-                                <summary>Parent</summary>
-                                <ul>
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                    </ul>
-                </details>
+        <p v-if="projectStore.noProjects" class="text-sm to-gray-500 mx-4">No hay proyectos</p>
+        <!-- menu -->
+        <ul v-else class="menu bg-base-200 rounded-box w-56">
+            <!--  -->
+            <li v-for="project in projectStore.projectList" :key="project.id">
+                <template v-if="project.tasks.length > 0">
+                    <details>
+                        <summary>
+                            <RouterLink :to="`/project/${project.id}`">
+                                {{ project.name }}
+                            </RouterLink>
+                        </summary>
+                        <ul>
+                            <li v-for="task in project.tasks" :key="task.id">
+                                <RouterLink :to="`/project/${project.id}`">{{
+                                    task.name
+                                }}</RouterLink>
+                            </li>
+                        </ul>
+                    </details>
+                </template>
+                <template v-else>
+                    <RouterLink :to="`/project/${project.id}`">
+                        {{ project.name }}
+                    </RouterLink>
+                </template>
             </li>
-            <li><a>Item 3</a></li>
         </ul>
     </aside>
     <!-- menu -->
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useProjectStore } from '../sotres/project.store';
+
+const projectStore = useProjectStore();
+</script>
 
 <style scoped></style>
